061c022a0807ec0dea347df3413672fe78d8d755
KimHyunLo <leeas016@gmail.com>
2022-11-30 15:39:32 +0900

    video call

---
+++ video_call/lib/const/agora.dart
@@ -0,0 +1,6 @@
+const APP_ID = 'd6b8366c87974917a5ed839f309e9e95';
+
+const CHANNEL_NAME = 'testChannel';
+
+const TEMP_TOKEN =
+    '007eJxTYJi2gGstx069q4+8PJO2T5jUuF5sytrynpOTJ+conz7yReSaAkOKWZKFsZlZsoW5pbmJpaF5omlqioWxZZqxgWUqEJruv9KW3BDIyHBe7yYzIwMEgvjcDCWpxSXOGYl5eak5DAwAoikj+A==';
